7a81c60089a81fb342f5cf3178ab4a9efbc56973
Srijana Thulung <srijana.thulung123@gmail.com>
2021-06-06 10:49:47 +0545

    added conditional return statement

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,14 +5,29 @@ import Card from '../UI/Card';
 import ExpenseFilter from '../Expenses/ExpenseFilter';
 
 const Expenses=(props)=> {
-    // console.log('this is apple');
-  
+  // console.log('this is apple');
   const [filteredYear, setFilteredYear] = useState('2019');
+  
   // const [filteredYear, setFilteredYear] = useState('2020');
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   }
   
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  });
+
+  let expensesContent = <p>No expense is found</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent= filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        titleAttribute={expense.title}
+        amountAttribute={expense.amount}
+        dateAttribute={expense.date}
+      />
+    ))
+  }
 
   return (
       //yeha className bhaney ko just a prop in this case coz yo custom component ho.
@@ -26,15 +41,7 @@ const Expenses=(props)=> {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          titleAttribute={expense.title}
-          amountAttribute={expense.amount}
-          dateAttribute={expense.date}
-        />
-      ))}
-      
+      {expensesContent}
 {/* </div> */}
       </Card>
 
